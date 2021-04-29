0f7e9f2d42746b3f3a2ed8bbea5295dd53460875
shadowsora211 <shadowsora@live.co.uk>
2021-04-29 19:17:38 +0100

    Show heatmap with csv data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,9 +10,18 @@
       </v-col>
     </v-row>
 
-    <v-row style="white-space: pre">
-      {{ filedata }}
+    <v-row v-if="historyMap" justify="center">
+        <calendar-heatmap :values="dailyCounts" :end-date="getToday()" tooltip-unit="titles" />
     </v-row>
+
+    <!-- <v-row style="white-space: pre">
+      {{ historyMap }}
+    </v-row>
+
+    <v-row style="white-space: pre">
+      {{ dailyCounts }}
+    </v-row> -->
+
   </v-container>
 </template>
 
@@ -24,7 +33,7 @@ export default {
 
   data: () => ({
     filename: null,
-    filedata: null,
+    historyMap: null,
   }),
 
   methods: {
@@ -32,10 +41,42 @@ export default {
       parseCSV(this.filename, (data) => {
         this.filedata = data;
         this.$toast.success("File uploaded!");
+        // convert fileData to dictonary
+        var map = {};
+        for (var i = 1; i < data.length; i++) {
+          // Date is the key
+          let date = data[i][1], title = data[i][0];
+          if (!date) continue;
+          if (!map[date]) map[date] = [];
+          map[date].push(title);
+        }
+        this.historyMap = map;
       }, (msg) => {
         this.$toast.error(msg);
       });
     },
+
+    formatDate(date) {
+      var parts = date.split("/");
+      return parts[2] + "-" + parts[1] + "-" + parts[0];
+    },
+
+    getToday() {
+      var currentDate = new Date()
+      return currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate();  
+    }
   },
+
+  computed: {
+    dailyCounts: function() {
+      if (!this.historyMap) return null;
+      var counts = [];
+      var dates = Object.keys(this.historyMap);
+      for (var i = 0; i < dates.length; i++) {
+        counts.push({ date: this.formatDate(dates[i]), count: this.historyMap[dates[i]].length });
+      }
+      return counts;
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueToast from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
+import VueCalendarHeatmap from 'vue-calendar-heatmap/dist/vue-calendar-heatmap.common'
+import 'vue-calendar-heatmap/dist/vue-calendar-heatmap.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueToast);
+Vue.use(VueCalendarHeatmap)
+
 
 new Vue({
   vuetify,
